443945f08db23a948210f336c676176b10519987
srinidhiseethala <119839619+srinidhiseethala@users.noreply.github.com>
2023-08-26 10:41:40 +0530

    import java.util.Scanner; public class Pattern {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int n,i,j;         n=sc.nextInt();         for(i=1;i<=n;i++)         {             for(j=1;j<=n;j++)             {                 if(i==j)                 {                     System.out.print("0");                 }                 else                 {                     System.out.print("x");                 }             }             System.out.println();         }     } }

---
+++ print_the_following_pattern_5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Pattern
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n,i,j;
+        n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                if(i==j)
+                {
+                    System.out.print("0");
+                }
+                else
+                {
+                    System.out.print("x");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
